afa435f427b6a979a8f9c1f9444ec050cdf78120
melfihry <fihrylara@gmail.com>
2024-05-18 04:49:53 +0100

    add interface

--- main/server.go
+++ main/server.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"ascii"
 )
@@ -34,8 +35,8 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	// }
 	text := r.FormValue("text")
 	Data := Info{
-		Title:       "AsciiArtWeb",
-		Description: "Make your Art",
+		Title:       "Ascii Art Web",
+		Description: "Convert text to ASCII art.",
 		Font1:       "standard",
 		Font2:       "shadow",
 		Font3:       "thinkertoy",
@@ -47,18 +48,17 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	// 	return
 	// }
 	err = templ.Execute(w, Data)
+	// make the text empty
 	Data.Text = ""
 	if err != nil {
 		log.Fatal(err)
 	}
+	os.WriteFile("../output/ascii-art-web.txt", []byte(Data.Content), 0o644)
 }
-
-// func AsciiArtWebHundler (w http.ResponseWriter,re *http.Request){
-// 	if
-// }
 
 func main() {
 	http.HandleFunc("/", pageHandler)
-	// http.Handle("/ascii-art", nil)
+	log.Println("\033[32mServer is running on port 8080...🚀\033[0m")
+	log.Println("\033[32mhttp://localhost:8080\033[0m")
 	http.ListenAndServe(":8080", nil)
 }
